8f08b4e9cf049f18c935a5215bedf2c88cf79c6f
Laser Kestrel <tidyandy@gmx.com>
2025-12-24 12:53:24 +0000

    Format Star.h for consistent code style
    windows compatible

--- src/Star.h
+++ src/Star.h
@@ -2,27 +2,30 @@
 #ifndef STAR_H
 #define STAR_H
 
+#include <cstdint>                // For uint32_t
 #include <SFML/Graphics/Color.hpp>
 #include <string>
+
 class Star
 {
 public:
-	Star(uint32_t ID, int x, int y, const std::string &name, const sf::Color &colour);
-	uint32_t getID() const;
-	int getX() const;
-	int getY() const;
-	std::string getName() const;
-	sf::Color getColour() const;
-	bool getIsExplored() const;
-	void setIsExplored(bool newIsExploredValue);
+    Star(uint32_t ID, int x, int y, const std::string &name, const sf::Color &colour);
+
+    uint32_t getID() const;
+    int getX() const;
+    int getY() const;
+    std::string getName() const;
+    sf::Color getColour() const;
+    bool getIsExplored() const;
+    void setIsExplored(bool newIsExploredValue);
 
 private:
-	uint32_t ID;
-	int x;
-	int y;
-	std::string name;
-	sf::Color colour;
-	bool isExplored;
+    uint32_t ID;
+    int x;
+    int y;
+    std::string name;
+    sf::Color colour;
+    bool isExplored;
 };
 
 #endif // STAR_H
